8c4eadee4a444390b877f256566ea8a2c374ac3f
Aaron France <aaron.l.france@gmail.com>
2015-01-04 14:05:21 +0100

    Implement skeletal ELB synchronizing

--- src/defcloud/elb.clj
+++ src/defcloud/elb.clj
@@ -1,6 +1,9 @@
 (ns defcloud.elb
-  (:require [defcloud.core :as defcloud]
-            [schema.core :as s]))
+  (:require [amazonica.aws.elasticloadbalancing :as elb]
+            [defcloud.core :as defcloud]
+            [schema.core :as s])
+  (:import [com.amazonaws.services.elasticloadbalancing.model
+            LoadBalancerNotFoundException]))
 
 
 (def ^:dynamic *default-availability-zones* ["us-east-1a"])
@@ -16,12 +19,39 @@
   {:name s/Str
    :listeners [listener-schema]
    :kind (s/enum :elb)
+   (s/optional-key :deps) [s/Any]
+   (s/optional-key :idle-timeout) s/Int
    (s/optional-key :availability-zones) az-schema})
 
 (defmethod defcloud/validate :elb
   [elb]
   (s/validate elb-schema elb))
 
+(defn schema->amazonica
+  "Converts from the API schema to the Amazonica names."
+  [{:keys [name availability-zones]
+    :or {availability-zones *default-availability-zones*} :as all}]
+  (merge all
+    {:availability-zones availability-zones
+     :load-balancer-name name}))
+
+(defn exists? [elb]
+  (let [name (or (:name elb) elb)]
+    (try
+      (elb/describe-load-balancer-attributes
+        :load-balancer-name name)
+      (catch LoadBalancerNotFoundException e
+        false))))
+
+(defn update-elb-settings [elb existing]
+  [elb existing])
+
 (defmethod defcloud/create-in-aws! :elb
   [elb]
-  true)
+  (let [elb (->> elb
+              (s/validate elb-schema)
+              schema->amazonica)
+        existing? (exists? elb)]
+    (if existing?
+      (update-elb-settings elb existing?)
+      (elb/create-load-balancer elb))))
